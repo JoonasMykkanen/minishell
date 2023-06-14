95610adec23f2b212ee0580ee4c6881f57520aab
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-06-14 08:47:49 +0300

    testing for leaks

--- src/init_helpers.c
+++ src/init_helpers.c
@@ -6,7 +6,7 @@
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 12:13:09 by joonasmykka       #+#    #+#             */
-/*   Updated: 2023/06/13 13:14:15 by joonasmykka      ###   ########.fr       */
+/*   Updated: 2023/06/14 08:47:38 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
