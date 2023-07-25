38ee36717e61e462169056ada9d5b75b6fb964e3
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-07-25 12:49:51 +0300

    there was a case where input was not freed when error flag was set

--- src/input/input.c
+++ src/input/input.c
@@ -6,7 +6,7 @@
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 18:19:52 by joonasmykka       #+#    #+#             */
-/*   Updated: 2023/07/08 10:51:06 by joonasmykka      ###   ########.fr       */
+/*   Updated: 2023/07/25 12:48:34 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ int	handle_input(char *input, t_data *data)
 		{
 			handle_multiline(&input, data);
 			if (g_sig_status == SIG_ERROR)
+			{
+				free(input);
 				return (1);
+			}
 		}
 		data->cur.raw = ft_strdup(input);
 		malloc_error_check(data->cur.raw, data);
